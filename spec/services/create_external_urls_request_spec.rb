6e0fc98235a2c4e947698b34a35c807ca7aaffb0
bombinsky <bombinsky@gmail.com>
2020-07-07 17:52:56 +0200

    Adds specs

--- spec/services/create_external_urls_request_spec.rb
+++ spec/services/create_external_urls_request_spec.rb
@@ -8,16 +8,40 @@ describe CreateExternalUrlsRequest do
 
   let(:attributes) do
     {
-      email: 'email@email.com',
+      email: email,
       start_time: Time.current - 1.day,
       end_time: Time.current
     }
   end
 
-  before do
-    allow_service_call(Publish, with: ['external_urls_requests', kind_of(String)])
+  before { allow_service_call(Publish, with: ['external_urls_requests', kind_of(String)]) }
+
+  context 'when valid attributes provided' do
+    let(:email) { 'email@domain.com' }
+
+    it { is_expected.to be_an ExternalUrlsRequest }
+    it { is_expected.to be_persisted }
+
+    it 'creates ExternalUrlsRequest' do
+      expect { service_call }.to change(ExternalUrlsRequest, :count).by(1)
+    end
+
+    it 'publish brand new request to external_urls_requests queue' do
+      request = service_call
+
+      expect(Publish).to have_received(:new).with('external_urls_requests', { 'request_id' => request.id }.to_json)
+    end
   end
 
-  it { is_expected.to be_an ExternalUrlsRequest }
-  it { is_expected.to be_persisted }
+  context 'when invalid attributes provided' do
+    let(:email) { 'email@domain' }
+
+    it { is_expected.to be_an ExternalUrlsRequest }
+    it { is_expected.not_to be_persisted }
+    it { is_expected.not_to be_valid }
+
+    it 'does not create ExternalUrlsRequest' do
+      expect { service_call }.not_to change(ExternalUrlsRequest, :count)
+    end
+  end
 end
